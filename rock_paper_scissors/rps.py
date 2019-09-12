e07a05cbbf3d32f0d26f62659e5e8b8172ab3313
joshua-disney <joshua.a.disney@gmail.com>
2019-09-12 14:47:50 -0700

    Start on rock_paper_scissors

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,50 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    # define list of possible plays (rock, paper, scissors)
+    options = ["rock", "paper", "scissors"]
+    # tracker variable integer
+    call_count = 0
+    # empty list to append to
+    possible_plays = [["rock"], ["paper"], ["scissors"]]
+
+    updatedPlays = []
+
+    if n == 0:
+        return []
+    elif n == 1:
+        print(options)
+        return options
+
+    def handle_list(arr):
+        # increment call count
+        print("array", arr)
+        nonlocal call_count
+        call_count += 1
+        for i in range(0, len(arr)):
+            for j in range(0, len(options)):
+                print(f"{options[j]}")
+                banana = possible_plays[i] + [options[j]]
+                print(f"possible plays: {possible_plays}")
+                updatedPlays.append(banana)
+                print(f"updatedPlays: {updatedPlays}")
+
+        if call_count == n:
+            return list
+        else:
+            handle_list(arr)
+
+    handle_list(possible_plays)
+
+
+rock_paper_scissors(3)
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
