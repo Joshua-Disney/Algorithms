1e5b75b071a80c62af0c1489f60671268f014c01
joshua-disney <joshua.a.disney@gmail.com>
2019-09-13 08:57:17 -0700

    Rock_Paper_Scissors recursion

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -6,38 +6,19 @@ import sys
 def rock_paper_scissors(n):
     # define list of possible plays (rock, paper, scissors)
     options = ["rock", "paper", "scissors"]
-    # tracker variable integer
-    call_count = 0
-    # empty list to append to
-    possible_plays = [["rock"], ["paper"], ["scissors"]]
-
+    # list that gets returned
     updatedPlays = []
 
-    if n == 0:
-        return []
-    elif n == 1:
-        print(options)
-        return options
-
-    def handle_list(arr):
-        # increment call count
-        print("array", arr)
-        nonlocal call_count
-        call_count += 1
-        for i in range(0, len(arr)):
-            for j in range(0, len(options)):
-                print(f"{options[j]}")
-                banana = possible_plays[i] + [options[j]]
-                print(f"possible plays: {possible_plays}")
-                updatedPlays.append(banana)
-                print(f"updatedPlays: {updatedPlays}")
-
-        if call_count == n:
-            return list
+    def recursiveHelp(count, list=[]):
+        if count > 0:
+            for i in options:
+                temp = list + [i]
+                recursiveHelp(count - 1, temp)
         else:
-            handle_list(arr)
-
-    handle_list(possible_plays)
+            updatedPlays.append(list)
+    recursiveHelp(n)
+    print(updatedPlays)
+    return updatedPlays
 
 
 rock_paper_scissors(3)
